23ec8365c6021327ad3e25bfce4ae39752f61a51
RenatoCRON <renato.cron@gmail.com>
2017-03-23 20:38:35 -0300

    die object instead of string

--- lib/Net/Flotum/API/ExceptionHandler.pm
+++ lib/Net/Flotum/API/ExceptionHandler.pm
@@ -43,11 +43,10 @@ sub request_with_retries {
             die "$msg\n";
         }elsif ( $res->code == 400 && ref $obj eq 'ARRAY' ) {
             my $msg = "Invalid data:\n";
-            $msg .= encode_json($_) for @{ $obj };
-
+            $msg .= encode_json($_) . "\n" for @{ $obj };
             $logger->error( &log_error_txt( $@, $req, $res ) );
             $logger->error($msg);
-            die "$msg\n";
+            die $obj;
         }
 
         $logger->error( &log_error_txt( $@, $req, $res ) );
